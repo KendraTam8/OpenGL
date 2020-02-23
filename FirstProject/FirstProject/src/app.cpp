498de45a6381f879c6a2c1174e7045cad1e8f37f
ktam <kendra.tam8@gmail.com>
2020-02-23 15:31:21 -0500

    add vertex buffers

--- FirstProject/FirstProject/src/app.cpp
+++ FirstProject/FirstProject/src/app.cpp
@@ -27,6 +27,17 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -35,9 +46,8 @@ int main(void)
         
         glBegin(GL_TRIANGLES);
 
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+        //(what you're drawing, which point to start with, number of vertices)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glEnd();
 
